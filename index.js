fa01ed84ff12df68768c7a1b489dcbedde666b5d
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-21 13:52:17 -0400

    Math Lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 16;
+const num4 = 4;
+
+const mod = 16 % 12; 
+
+const max = Math.max(5, 10, 15, 20); 
